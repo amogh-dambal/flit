84fc163972543e896cb964747a45e9d8f0fb5f50
amogh <amdam2013@gmail.com>
2022-05-26 19:15:24 -0500

    cleanup

--- test/test-main.c
+++ test/test-main.c
@@ -3,15 +3,10 @@
 #include "../inc/input.h"
 #include <sys/stat.h>
 #include <string.h>
+#include <errno.h>
+
 
 int main(int argc, char** argv) {
-
-    const char* src = "test";
-    char dest[100];
-
-    size_t n = strlcpy(dest, src, 5);
     
-    printf("arg: %d\n", 5);
-    printf("Copied %u bytes into %s from %s\n", n, dest, src);
     return 0;
 }
